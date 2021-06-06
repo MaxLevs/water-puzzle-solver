e6d96029c2a707bdac5c12b8fb7c275e53ab3b7a
oespino <oespino88@gmail.com>
2021-06-06 22:36:23 +0200

    Improve input. Changes styles.

--- components/selector.js
+++ components/selector.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import styles from './selector.module.css'
 
 export default function Selector({ onClick }) {
 
@@ -12,12 +13,14 @@ export default function Selector({ onClick }) {
     return (
         <div>
             <span>Número total de tubos de ensayo</span>
-            <input type='text' onChange={event => setTotalNumber(event.target.value)} ></input>
+            <input type='text' className={styles.input} onChange={event => setTotalNumber(event.target.value)} ></input>
             <br />
             <span>Número de tubos de ensayo vacíos</span>
-            <input type='text' onChange={event => setEmptyNumber(event.target.value)}></input>
+            <input type='text' className={styles.input} onChange={event => setEmptyNumber(event.target.value)}></input>
             <br />
-            <button disabled={!totalNumber || !emptyNumber} onClick={() => handleClick()}>Go</button>
+            <div className={styles.buttonContainer}>
+                <button className={styles.button} disabled={!totalNumber || !emptyNumber} onClick={() => handleClick()}>Go</button>
+            </div>
         </div>
     )
 
--- pages/index.js
+++ pages/index.js
@@ -4,54 +4,11 @@ import Tube from '../components/tube'
 import State from '../components/state'
 import Solution from '../components/solution'
 import Selector from '../components/selector'
+import ColorSelector from '../components/colorSelector'
 import styles from '../styles/Home.module.css'
 import stateLib from '../lib/states'
 
 export default function Home() {
-
-  var tubes1 = [
-    ["1", "2", "3", "4"],
-    ["5", "6", "6", "5"],
-    ["7", "3", "8", "9"],
-    ["8", "4", "1", "10"],
-    ["3", "10", "9", "11"],
-    ["12", "12", "2", "9"],
-    ["8", "3", "11", "4"],
-    ["7", "6", "5", "10"],
-    ["9", "12", "4", "1"],
-    ["11", "7", "1", "8"],
-    ["6", "2", "5", "11"],
-    ["12", "10", "2", "7"],
-    [],
-    []
-  ];
-
-  let colors = [
-    "red",
-    "blue",
-    "palegreen",
-    "rebeccapurple",
-    "grey",
-    "orange",
-    "salmon",
-    "maroon",
-    "green",
-    "lemonchiffon",
-    "lightskyblue",
-    "yellowgreen"
-  ]
-
-  var tubes2 = tubes1.map((tube) => {
-    return tube.map((number) => colors[number - 1])
-  })
-
-  //console.log(tubes2)
-
-  //let history = [];
-  //history.push(tubes2)
-
-  //let { stackI, stackJ } = stateLib.resolve(history)
-
   const PAGE_STATUSES = {
     NUMBER_INPUT: 0,
     COLOR_INPUT: 1,
@@ -59,7 +16,7 @@ export default function Home() {
   }
   const [totalNumber, setTotalNumber] = useState('')
   const [emptyNumber, setEmptyNumber] = useState('')
-  const [color, setColor] = useState(colors[0])
+  const [color, setColor] = useState("black")
   const [tubes, setTubes] = useState([])
   const [history, setHistory] = useState([])
   const [pageStatus, setPageStatus] = useState(PAGE_STATUSES.NUMBER_INPUT)
@@ -78,12 +35,12 @@ export default function Home() {
 
   const handleClick = function (index, color) {
     let tubesCopy = tubes.slice()
-    if (tubesCopy[index].length < 4) tubesCopy[index].push(color)
+    if (color !== "black") {
+      if (tubesCopy[index].length < 4) tubesCopy[index].push(color)
+    } else {
+      tubesCopy[index].pop()
+    }
     setTubes(tubesCopy)
-  }
-
-  const handleChangeColor = function (event) {
-    setColor(event.target.value)
   }
 
   const solvePuzzle = function () {
@@ -107,14 +64,12 @@ export default function Home() {
         return (
           <>
             <State tubes={tubes} numberOfReadOnly={emptyNumber} colorSelected={color} onClick={handleClick} />
-            <select name="select" onChange={handleChangeColor} style={{ color: color }}>
-              {colors.map((el, index) => {
-                return (
-                  <option key={index} value={el} style={{ color: el }}>&#9632;</option>
-                )
-              })}
-            </select>
-            <button onClick={solvePuzzle}>Resolver</button>
+            <div className={styles.colorSelectionContainer} >
+              <h1>Tu selección: </h1>
+              <div className={styles.colorSelection} style={{ backgroundColor: color }}></div>
+            </div>
+            <ColorSelector selectColor={setColor} />
+            < button className={styles.button} onClick={solvePuzzle}>Resolver</button>
           </>
         )
       case PAGE_STATUSES.SOLUTION_OUTPUT:
@@ -128,6 +83,8 @@ export default function Home() {
       <Head>
         <title>Water Sort Puzzle Solver</title>
         <meta name="description" content="Webapp to solve these tricky puzzles and laught at your friends" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
